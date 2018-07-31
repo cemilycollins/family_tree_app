64fc2090335ab6a9a855f11d2f5e2fa144881580
Ruby Weiner <rubyweiner796@me.com>
2018-07-31 15:43:07 -0400

    people controller actions

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,50 @@
 class PeopleController < ApplicationController
+  before_action :person, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @people = Person.all
+  end
+
+  def show
+  end
+
+  def new
+    @person = Person.new
+  end
+
+  def create
+    @person = Person.new(person_params)
+    if @person.save
+      redirect_to person_path(@person)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @person.update(person_params)
+      redirect_to person_path(@person)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @person.destroy
+    redirect_to people_path
+  end
+
+private
+
+  def person_params
+    params.require(:person).permit(:first_name, :last_name, :dob, :dod, :deceased, :gender, :place_of_birth, :current_location)
+  end
+
+  def person
+    @person = Person.find(params[:id])
+  end
+
 end
