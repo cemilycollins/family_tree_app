47a4b2772ae85e2fe449e28863825c446e477438
Ruby Weiner <rubyweiner796@me.com>
2018-07-31 16:33:56 -0400

    partnership table

---
+++ db/migrate/20180731202321_partnership.rb
@@ -0,0 +1,10 @@
+class Partnership < ActiveRecord::Migration[5.2]
+  def change
+    create_table :partnerships do |t|
+      t.integer :person_id
+      t.integer :partner_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_31_183432) do
+ActiveRecord::Schema.define(version: 2018_07_31_202321) do
 
   create_table "ethnicities", force: :cascade do |t|
     t.string "type"
     t.integer "person_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "partnerships", force: :cascade do |t|
+    t.integer "person_id"
+    t.integer "partner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
