e0fc10a2ec75ad2cc90bb923ec9b4696d9f31d65
Ruby Weiner <rubyweiner796@me.com>
2018-08-01 18:02:48 -0400

    add photo to family photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,33 @@
 class PhotosController < ApplicationController
   def index
-    @photos = Photo.all
+    @family = Family.find(params[:family_id])
+    @photos = @family.photos
   end
+
+  def show
+    @family = Family.find(params[:family_id])
+    @photo = Photo.find(params[:id])
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @family = Family.find(params[:family_id])
+    @photo = Photo.create(photo_params)
+    if @photo.valid?
+      @photo.save
+      redirect_to family_photos_path(@family)
+    else
+      render :new
+    end
+  end
+
+private
+
+  def photo_params
+    params.require(:photo).permit(:name, :img_url, :family_id)
+  end
+
 end
