251c1effdfb309e6f859032012573580c721b85a
Ruby Weiner <rubyweiner796@me.com>
2018-08-01 15:31:26 -0400

    create new family

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,6 +1,7 @@
 class Family < ApplicationRecord
   has_many :people
   has_many :photos
+  accepts_nested_attributes_for :photos
 
   def family_portrait
     self.photos.find {|photo| photo.photo_type = "family portrait"}.img_url
