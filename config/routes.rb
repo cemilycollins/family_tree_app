c5bb98f9cdef9b8a62d505e8e546e6d9ae78a6b6
github username <github email address>
2018-08-17 13:57:35 -0400

    added home route

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   get '/sign_in', to: 'sessions#new'
   post '/sign_in', to: 'sessions#create'
   delete '/sign_out', to: 'sessions#destroy'
+  get '/' to: 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
